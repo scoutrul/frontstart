4d338e95642f90ca054c916476e030187ab1c432
Antony <antongolova@gmail.com>
2019-01-08 01:05:56 +0300

    redux devtools on client

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import rootReducer from './reducers'
+
+const isClient = process.browser;
+const composeEnhancers = isClient ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
-    preloadedState
+    preloadedState,
+    composeEnhancers()
   )
 }
+
+
